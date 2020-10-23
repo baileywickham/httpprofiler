1da293ca1a0c1588c11f8fb56f4f07e2802ff464
baileywickham <baileywickham@gmail.com>
2020-10-23 14:43:02 -0700

    add keepalive

--- profiler.go
+++ profiler.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net"
 	"net/url"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -15,6 +17,11 @@ type Profile struct {
 	rsp     HTTPResponse
 }
 
+var _url = flag.String("url", "http://cloudflare.com", "URL to profile")
+var n = flag.Int("profile", 1, "Number of requests to send")
+var keepalive = flag.Bool("keepalive", false, "Attempt to use a keepalive request to use the same TCP connection")
+var v = flag.Bool("v", false, "Print responses as they are recieved")
+
 func parseURL(_url string) url.URL {
 	// Parse the URL into a URL struct
 	u, err := url.Parse(_url)
@@ -24,87 +31,107 @@ func parseURL(_url string) url.URL {
 	return *u
 }
 
-func startProfile(u url.URL, n int, v bool) []Profile {
+func startProfileKeepAlive(u url.URL) []Profile {
 	// profile with a single TCP request
-	profs := make([]Profile, n)
+	profs := make([]Profile, *n)
 
 	// Open a tcp connection with the url requsted. Use http or https
 	conn, err := net.Dial("tcp", u.Hostname()+":"+u.Scheme)
+	defer conn.Close()
 	if err != nil {
 		panic(err)
 	}
 
-	// Create a HTTP get request
-	request := createGetRequest(u)
-	for i := 0; i <= n; i++ {
+	// Create a HTTP get request, true indicates the keepalive header should
+	// be included
+	request := createGetRequest(u, true)
+	for i := 0; i < *n; i++ {
 		start := time.Now()
 		// Send that request
 		fmt.Fprintf(conn, request)
 		rsp := readResponse(conn)
+		duration := time.Since(start)
 		p := Profile{
 			n:       i,
-			rspTime: time.Since(start),
+			rspTime: duration,
 			rsp:     rsp}
-		if v {
-			printRsp(rsp, n)
+		if *v {
+			printRsp(rsp, i, duration)
 		}
 		profs[i] = p
+
+		// Stop on closed connection
+		if strings.Contains(rsp.GeneralHeaders+rsp.ResponseHeaders+rsp.EntityHeaders,
+			"Connection: close") {
+			panic("Connection closed, keepalive response not sent")
+		}
 	}
 	return profs
 }
 
-func startProfileTCP(u url.URL, n int, v bool) []Profile {
+func startProfile(u url.URL) []Profile {
 	// startProfileTCP includes the time of the TCP connection,
 	// not only the http request
-	profs := make([]Profile, n)
+	profs := make([]Profile, *n)
 
-	for i := 0; i <= n; i++ {
+	for i := 0; i < *n; i++ {
 		conn, err := net.Dial("tcp", u.Hostname()+":"+u.Scheme)
 		defer conn.Close()
 		if err != nil {
 			panic(err)
 		}
-		// Create a HTTP get request
-		request := createGetRequest(u)
+		// Create a HTTP get request, no keepalive
+		request := createGetRequest(u, false)
 		start := time.Now()
 		// Send that request
 		fmt.Fprintf(conn, request)
 		rsp := readResponse(conn)
+		duration := time.Since(start)
 		p := Profile{
 			n:       i,
-			rspTime: time.Since(start),
+			rspTime: duration,
 			rsp:     rsp}
-		if v {
-			printRsp(rsp, n)
+		if *v {
+			printRsp(rsp, i, duration)
 		}
 		profs[i] = p
 	}
 	return profs
 }
 
-func printRsp(rsp HTTPResponse, n int) {
-
+func printRsp(rsp HTTPResponse, n int, t time.Duration) {
+	fmt.Printf("Request number: %d\nStatus Code: %s\nResponse time: %dms\n",
+		n, rsp.StatusCode, t.Milliseconds())
 }
 
-func evaluate(profs []Profile, n int) {
-	//times := make([]time.Duration, n)
-	//for i, p := range profs {
-	//	times[i] = p.rspTime
-	//}
+func evaluate(profs []Profile) {
+	// A success response is a 200 response
+	sucesses := 0
+	times := make([]time.Duration, n)
+	errors := make([]string, 0)
+
+	fmt.Println("--------------------------------------------------------------------------------")
+	fmt.Printf("Number of requests: %d\n", *n)
+
+	for i, p := range profs {
+		code, _ := strconv.Atoi(p.rsp.StatusCode)
+		if code >= 200 && code < 300 {
+			sucesses++
+		} else {
+			errors = append(errors, p.rsp.StatusCode)
+		}
+		times[i] = p.rspTime
+	}
 }
 
 func main() {
-	_url := flag.String("url", "http://cloudflare.com", "URL to profile")
-	n := flag.Int("profile", 1, "Number of requests to send")
-	tcp := flag.Bool("tcp", false, "Start a new tcp connection between requests?")
-	v := flag.Bool("v", false, "Print responses as they are recieved")
 	flag.Parse()
 
 	u := parseURL(*_url)
 
-	if *tcp {
-		evaluate(startProfileTCP(u, *n, *v), *n)
+	if *keepalive {
+		evaluate(startProfileKeepAlive(u))
 	} else {
-		evaluate(startProfile(u, *n, *v), *n)
+		evaluate(startProfile(u))
 	}
 }
