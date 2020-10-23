1cf84b80481aa52e71248da8b6b044f89ab14d26
baileywickham <baileywickham@gmail.com>
2020-10-22 21:34:44 -0700

    add http structs

--- http.go
+++ http.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
+	"net"
+	"net/url"
+	"strings"
 )
 
 type HTTPRequest struct {
@@ -9,6 +14,7 @@ type HTTPRequest struct {
 	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
 	// Defining this struct allows more flexibility over just a get request,
 	// and can be extended to test other request types.
+
 	// Start-line
 	HTTPMethod    string
 	RequestTarget string
@@ -21,22 +27,89 @@ type HTTPRequest struct {
 	Body string
 }
 
-func (r *HTTPRequest) String() string {
-	return fmt.Sprintf("%s %s %s\r\n%s%s%s\r\n\r\n%s",
-		r.HTTPMethod, r.RequestTarget, r.HTTPVersion,
-		r.GeneralHeaders, r.RequestHeaders, r.EntityHeaders,
-		r.Body)
+type HTTPResponse struct {
+	// An http repsonse found here
+	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
+
+	// Start-line
+	HTTPVersion string
+	StatusCode  string
+	StatusText  string
+	// Headers
+	GeneralHeaders  string
+	ResponseHeaders string
+	EntityHeaders   string
+	// Body
+	Body string
+	Size int
 }
 
-func createGetRequest(url string) string {
+func (r *HTTPRequest) String() string {
+	s := fmt.Sprintf("%s %s %s\r\n", r.HTTPMethod, r.RequestTarget, r.HTTPVersion)
+	//%s%s%s\r\n\r\n%s,
+	if r.GeneralHeaders != "" {
+		s += r.GeneralHeaders
+	}
+	if r.RequestHeaders != "" {
+		s += r.RequestHeaders
+	}
+	if r.EntityHeaders != "" {
+		s += r.EntityHeaders
+	}
+	s += "\r\n\r\n"
+	if r.Body != "" {
+		s += r.Body
+	}
+	return s
+}
+
+func createGetRequest(u url.URL) string {
 	r := HTTPRequest{
 		HTTPMethod:     "GET",
-		RequestTarget:  "/", //url,
+		RequestTarget:  u.RequestURI(), //url,
 		HTTPVersion:    "HTTP/1.0",
-		GeneralHeaders: "",
+		GeneralHeaders: fmt.Sprintf("Host: %s", u.Hostname()),
 		RequestHeaders: "",
 		EntityHeaders:  "",
 		Body:           "",
+		Size:           0,
 	}
 	return r.String()
+}
+
+func readResponse(conn net.Conn) HTTPResponse {
+	r := bufio.NewReader(conn)
+	_startline, err := r.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+
+	startline := strings.Fields(_startline)
+
+	rsp := HTTPResponse{
+		HTTPVersion: startline[0],
+		StatusCode:  startline[1],
+		StatusText:  startline[2],
+	}
+	for {
+		line, err := r.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		if line == "\r\n" {
+			break
+		}
+		//if strings.Contains(line, "Content-Length:") {
+		//	length := strings.Fields(line)[1]
+		//	println(length)
+		//}
+		rsp.GeneralHeaders += line
+	}
+	body, err := ioutil.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
+	rsp.Size = len(body)
+	rsp.Body = string(body)
+	return rsp
 }
