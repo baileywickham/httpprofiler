66e3eae6958d2c1a5f35ad38f0510054f4c0adef
baileywickham <baileywickham@gmail.com>
2020-10-23 22:46:51 -0700

    add helpers

---
+++ helpers.go
@@ -0,0 +1,30 @@
+package main
+
+import "time"
+
+func MinMaxDuration(array []time.Duration) (time.Duration, time.Duration) {
+	var max time.Duration = array[0]
+	var min time.Duration = array[0]
+	for _, value := range array {
+		if max < value {
+			max = value
+		}
+		if min > value {
+			min = value
+		}
+	}
+	return min, max
+}
+func MinMaxInt(array []int) (int, int) {
+	var max int = array[0]
+	var min int = array[0]
+	for _, value := range array {
+		if max < value {
+			max = value
+		}
+		if min > value {
+			min = value
+		}
+	}
+	return min, max
+}
